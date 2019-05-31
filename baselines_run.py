5767b918ea0289fee947a11ca5ee02bf38444d46
Pinaki Gupta <pinaki.gupta@havalus.com>
2019-05-31 09:44:55 -0400

    Updated file model and output file handling stuff

--- baselines_run.py
+++ baselines_run.py
@@ -4,7 +4,12 @@
 #                      Author: Munir Jojo-Verge
 #######################################################################
 
-import sys, os
+from settings import req_dirs, models_folder
+import urban_env
+from baselines import logger
+import baselines.run as run
+import sys
+import os
 from os.path import dirname, abspath
 import time
 import gym
@@ -14,9 +19,10 @@ import warnings
 import subprocess
 from baselines.common.cmd_util import common_arg_parser, parse_unknown_args
 from baselines.results_plotter import plot_results
-from baselines.common.vec_env import  VecEnv
+from baselines.common.vec_env import VecEnv
 from baselines.common import tf_util
 import tensorflow as tf
+from shutil import copyfile
 
 ####################
 pathname = os.getcwd()
@@ -26,144 +32,154 @@ foldername = os.path.basename(pathname)
 ####################
 
 open_ai_baselines_dir = pathname + '/open_ai_baselines'
-#print(open_ai_baselines_dir)
+# print(open_ai_baselines_dir)
 
 urban_AD_env_path = pathname + '/urban_env/envs'
-#print(urban_AD_env_path)
+# print(urban_AD_env_path)
 
 sys.path.append(open_ai_baselines_dir)
 sys.path.append(urban_AD_env_path)
 
-import baselines.run as run
-from baselines import logger
 
-import urban_env
-
-from settings import req_dirs, models_folder
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-warnings.filterwarnings("ignore") 
-
-
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.filterwarnings("ignore")
 
 
 ###############################################################
-#        DEFINE YOUR "BASELINE" (AGENT) PARAMETERS HERE 
+#        DEFINE YOUR "BASELINE" (AGENT) PARAMETERS HERE
 ###############################################################
-train_env_id =  'two-way-v0' 
+train_env_id = 'two-way-v0'
 play_env_id = 'two-way-v0'
 alg = 'ppo2'
 network = 'mlp'
-num_timesteps = '1e0'
+num_timesteps = '1e1'
 #################################################################
+first_call = True
+
 
 def create_dirs(req_dirs):
     for dirName in req_dirs:
         if not os.path.exists(dirName):
             os.mkdir(dirName)
             #print("Directory " , dirName ,  " Created ")
-        #else:    
+        # else:
             #print("Directory " , dirName ,  " already exists")
+
 
 InceptcurrentDT = time.strftime("%Y%m%d-%H%M%S")
 
+
 def is_predict_only():
-    return float(num_timesteps)==1
+    return float(num_timesteps) == 1
 
-def default_args(save_in_sub_folder=None):    
+def default_args(save_in_sub_folder=None):
     create_dirs(req_dirs)
-
     currentDT = time.strftime("%Y%m%d-%H%M%S")
-    
+    global first_call
     ####################################################################
-    # DEFINE YOUR SAVE FILE, LOAD FILE AND LOGGING FILE PARAMETERS HERE 
-    #################################################################### 
-    save_folder = pathname + '/' + models_folder + '/' + train_env_id +'/'+ alg + '/' + network 
-    if save_in_sub_folder is not None:
-       save_folder +=  '/' + str(save_in_sub_folder)
-    save_file = save_folder + '/' + str(currentDT)
-    list_of_file = glob.glob(save_folder+'/*')
+    # DEFINE YOUR SAVE FILE, LOAD FILE AND LOGGING FILE PARAMETERS HERE
+    ####################################################################
+    save_folder = pathname + '/' + models_folder + \
+        '/' + train_env_id + '/' + alg + '/' + network
 
-    # Specifiy log directories for open AI 
+    if first_call:
+        list_of_file = glob.glob(save_folder+'/*') 
+        if save_in_sub_folder is not None:
+            save_folder += '/' + str(save_in_sub_folder)
+        save_file = save_folder + '/' + str(currentDT)
+        first_call = False
+    else:       
+        if save_in_sub_folder is not None:
+            save_folder += '/' + str(save_in_sub_folder)
+        save_file = save_folder + '/' + str(currentDT)
+        list_of_file = glob.glob(save_folder+'/*')
+
+    # Specifiy log directories for open AI
     '''logger_path = save_folder + '/log/'
     tb_logger_path = save_folder + '/tb/'
     os.environ['OPENAI_LOGDIR'] = logger_path
     os.environ['OPENAI_LOG_FORMAT'] = 'stdout,tensorboard'''
-  
+
     ###############################################################
-        
-    try:  
+
+    try:
         os.mkdir(save_folder)
-    except OSError:  
+    except OSError:
         #print ("Creation of the save path %s failed. It might already exist" % save_folder)
-        a=1
-    else:  
-        print ("Successfully created the save path folder %s " % save_folder)
+        a = 1
+    else:
+        print("Successfully created the save path folder %s " % save_folder)
 
     DEFAULT_ARGUMENTS = [
         '--env=' + train_env_id,
         '--alg=' + alg,
         '--network=' + network,
-        '--num_timesteps=' + num_timesteps,    
-    #    '--num_env=0',
-    #    '--save_path=' + save_file,        
-    #     '--tensorboard --logdir=' + tb_logger_path,
-    #    '--play'
-    #    '--num_env=8' 
+        '--num_timesteps=' + num_timesteps,
+        #    '--num_env=0',
+        #    '--save_path=' + save_file,
+        #     '--tensorboard --logdir=' + tb_logger_path,
+        #    '--play'
+        #    '--num_env=8'
     ]
 
     try:
         from mpi4py import MPI
         rank = MPI.COMM_WORLD.Get_rank()
-        print("rank",rank)
+        print("rank", rank)
     except ImportError:
         MPI = None
 
-    def save_model(save_file = None):
+    def save_model(save_file=None):
         if save_file is not None:
             if MPI is None or rank == 0:
                 DEFAULT_ARGUMENTS.append('--save_path=' + save_file)
-                print("Saving file",save_file)
+                print("Saving file", save_file)
                 #DEFAULT_ARGUMENTS.append('--tensorboard --logdir=' + tb_logger_path)
-        return 
+        return
 
-    def load_model(load_file = None):
+    def load_model(load_file=None):
         if load_file is not None:
             DEFAULT_ARGUMENTS.append('--load_path=' + load_file)
-            print("Loading file",load_file)
-        return 
+            print("Loading file", load_file)
+        return
 
+    def copy_terminal_output_file():
+        src = os.getcwd()+'/output.txt'
+        dst = save_folder+'/output.txt'
+        copyfile(src, dst)
 
-    if list_of_file: # is there anything in the save directory
-        latest_file_or_folder = max( list_of_file, key=os.path.getctime) 
+    if list_of_file:  # is there anything in the save directory
+        latest_file_or_folder = max(list_of_file, key=os.path.getctime)
 #        print(" latest_file_or_folder " ,latest_file_or_folder)
-        if os.path.isdir(latest_file_or_folder): # search for the latest file (to load) from the latest folder\
+        if os.path.isdir(latest_file_or_folder):  # search for the latest file (to load) from the latest folder\
             latest_folder = latest_file_or_folder
             list_of_files = glob.glob(latest_folder+'/*')
-            if list_of_files and (save_in_sub_folder is not None) :
-               if save_in_sub_folder in latest_folder:
-                   while list_of_files:
-                        latest_file = max( list_of_files, key=os.path.getctime)
-                        if os.path.isfile(latest_file) :
+            if list_of_files and (save_in_sub_folder is not None):
+                if save_in_sub_folder in latest_folder:
+                    while list_of_files:
+                        latest_file = max(list_of_files, key=os.path.getctime)
+                        if os.path.isfile(latest_file) and ('output' not in latest_file):
                             load_model(load_file=latest_file)
-                            print(" load_path " ,latest_file)
+                            print(" load_path ", latest_file)
                             break
-                        list_of_files.remove(latest_file)                           
+                        list_of_files.remove(latest_file)  # directory
             save_model(save_file=save_file)
+            copy_terminal_output_file()
 #            print(" save_path " ,save_file)
-        else: # got the latest file (to load)
+        else:  # got the latest file (to load)
             latest_file = latest_file_or_folder
             if is_predict_only():
-                load_model(load_file=latest_file) 
-            elif save_in_sub_folder is not None :
-                load_model(load_file=latest_file) 
+                load_model(load_file=latest_file)
+            elif save_in_sub_folder is not None:
+                load_model(load_file=latest_file)
                 save_model(save_file=save_file)
+                copy_terminal_output_file()
             else:
                 save_model(save_file=save_file)
-    else : 
+    else:
         print(" list_of_file empty in load path ", save_folder)
         if not is_predict_only():
-                save_model(save_file=save_file)
+            save_model(save_file=save_file)
 
     #print(" DEFAULT_ARGUMENTS ", DEFAULT_ARGUMENTS)
 
@@ -171,9 +187,9 @@ def default_args(save_in_sub_folder=None):
 
 
 def play(env, policy):
-    
+
     # env = gym.make(env_id)
-    
+
     logger.configure()
     #logger.log("Running trained model")
     obs = env.reset()
@@ -185,7 +201,7 @@ def play(env, policy):
     episode_len = 0
     while True:
         if state is not None:
-            actions, _, state, _ = policy.step(obs,S=state, M=dones)
+            actions, _, state, _ = policy.step(obs, S=state, M=dones)
         else:
             actions, _, _, _ = policy.step(obs)
 
@@ -194,11 +210,11 @@ def play(env, policy):
         episode_len += 1
         env.render()
         done = done.any() if isinstance(done, np.ndarray) else done
-        #if episode_len%100 ==0:
-            #print('episode_rew={}'.format(episode_rew), '  episode_len={}'.format(episode_len))
+        # if episode_len%100 ==0:
+        #print('episode_rew={}'.format(episode_rew), '  episode_len={}'.format(episode_len))
         if done:
-            print('episode_rew={}'.format(episode_rew), '  episode_len={}'.format(episode_len),\
-                'episode travel = ',env.vehicle.position[0]-env.ego_x0)
+            print('episode_rew={}'.format(episode_rew), '  episode_len={}'.format(episode_len),
+                  'episode travel = ', env.vehicle.position[0]-env.ego_x0)
             episode_rew = 0
             episode_len = 0
             env.close()
@@ -206,17 +222,15 @@ def play(env, policy):
 
 
 if __name__ == "__main__":
-    
-
 
     itr = 1
     sys_args = sys.argv
 
     policy = None
     play_env = None
-    max_iteration = 1  
+    max_iteration = 5
     if not is_predict_only():
-        while itr<=max_iteration:
+        while itr <= max_iteration:
             sess = tf_util.get_session()
             sess.close()
             tf.reset_default_graph()
@@ -226,7 +240,7 @@ if __name__ == "__main__":
                 save_in_sub_folder = None
                 if max_iteration > 1:
                     save_in_sub_folder = InceptcurrentDT
-                args = default_args(save_in_sub_folder)
+                args = default_args(save_in_sub_folder=save_in_sub_folder)
 
             policy = run.main(args)
 
@@ -234,11 +248,11 @@ if __name__ == "__main__":
             itr += 1
 
             try:
-                play(play_env,policy)
+                play(play_env, policy)
             except Exception as e:
-                print("Could not play the prediction after training due to error  ",e)
+                print("Could not play the prediction after training due to error  ", e)
             #from tensorboard import main as tb
-            #tb.main()
+            # tb.main()
 
     else:
         policy = run.main(default_args())
@@ -252,9 +266,9 @@ if __name__ == "__main__":
     try:
         # Just try to Play
         while True:
-            play(play_env,policy)
+            play(play_env, policy)
             '''sess = tf_util.get_session()
             sess.close()
             tf.reset_default_graph()'''
     except Exception as e:
-        print("Could not play the prediction due to error ",e)
+        print("Could not play the prediction due to error ", e)
