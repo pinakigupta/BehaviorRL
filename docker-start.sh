9738580c6d2c22a1a3c8ed6fdd4734b65e31683b
Ubuntu <ubuntu@ip-172-31-62-11.us-west-2.compute.internal>
2019-06-17 20:15:13 +0000

    Update docker start code to identify if this is an EC2 instance (Ubuntu OR AmazonAMI both) and call for xwindows accordingly.

--- docker-start.sh
+++ docker-start.sh
@@ -79,12 +79,27 @@ else
 fi
 
 
+EC2Instance=false
+# This first, simple check will work for many older instance types.
+if [ -f /sys/hypervisor/uuid ]; then
+  # File should be readable by non-root users.
+  if [ `head -c 3 /sys/hypervisor/uuid` == "ec2" ]; then
+    EC2Instance=true
+  fi
+# This check will work on newer m5/c5 instances, but only if you have root!
+elif [ -r /sys/devices/virtual/dmi/id/product_uuid ]; then
+  # If the file exists AND is readable by us, we can rely on it.
+  if [ `head -c 3 /sys/devices/virtual/dmi/id/product_uuid` == "EC2" ]; then
+    EC2Instance=true
+  fi
+fi
+
 
 
 # We are ready to run the main docker container
-if [ "$DISTRO" == "AmazonAMI" ]; then
+if [ $EC2Instance ]; then
         echo "docker run Amazon AMI version"
-	sudo docker run -it --runtime=nvidia -v $HOST_DIR:/rl_baselines_ad -v /tmp/.X11-unix:/tmp/.X11-unix munirjojoverge/rl_baselines /bin/bash
+	sudo docker run -it --runtime=nvidia -v $HOST_DIR:/rl_baselines_ad munirjojoverge/rl_baselines /bin/bash
 else
         echo "docker run Ubuntu version"
 	xhost +
