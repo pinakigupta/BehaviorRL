7fe5d72e8683b6c2f21637a6e05f29324b9e308f
pinakigupta <pinaki.gupta@havalus.com>
2019-07-18 09:50:06 -0400

    Seperated ray related methods/classes in a new file raylibs.py

--- baselines_run.py
+++ baselines_run.py
@@ -1,8 +1,3 @@
-######################################################################
-#          Deep Reinforcement Learning for Autonomous Driving
-#                  Created/Modified on: February 5, 2019
-#                      Author: Munir Jojo-Verge
-#######################################################################
 import urban_env
 from urban_env.envs.two_way_env import TwoWayEnv
 from urban_env.envs.abstract import AbstractEnv
@@ -22,10 +17,6 @@ import os
 from os.path import dirname, abspath
 import time
 import pprint
-import ray
-from ray.tune import Experiment, run_experiments, register_env, sample_from
-from ray.tune.schedulers import PopulationBasedTraining, AsyncHyperBandScheduler
-from ray.tune.ray_trial_executor import RayTrialExecutor
 pp = pprint.PrettyPrinter(indent=4)
 ####################
 
@@ -55,6 +46,8 @@ gym.Env.metadata['_predict_only'] = is_predict_only()
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.filterwarnings("ignore")
 
+import ray
+from ray.tune import register_env
 register_env(train_env_id, lambda config: TwoWayEnv(config))
 redis_add = ray.services.get_node_ip_address() + ":6379"
 try:
@@ -121,112 +114,6 @@ def play(env, policy):
             break
 
 
-def explore(config):
-        # ensure we collect enough timesteps to do sgd
-        if config["train_batch_size"] < config["sgd_minibatch_size"] * 2:
-            config["train_batch_size"] = config["sgd_minibatch_size"] * 2
-        # ensure we run at least one sgd iter
-        if config["num_sgd_iter"] < 1:
-            config["num_sgd_iter"] = 1
-        return config
-
-
-pbt = PopulationBasedTraining(
-                                        time_attr="training_iteration",
-                                        metric="episode_reward_mean",
-                                        mode="max",
-                                        perturbation_interval=7,
-                                        resample_probability=0.25,
-                                        # Specifies the mutations of these hyperparams
-                                        hyperparam_mutations={
-                                                                "lambda": lambda: random.uniform(0.9, 1.0),
-                                                                "clip_param": lambda: random.uniform(0.01, 0.5),
-                                                                "lr": [1e-3, 5e-4, 1e-4, 5e-5, 1e-5],
-                                                                "num_sgd_iter": lambda: random.randint(1, 30),
-                                                                "sgd_minibatch_size": lambda: random.randint(128, 16384),
-                                                                "train_batch_size": lambda: random.randint(2000, 160000),
-                                                             },
-                                        custom_explore_fn=explore
-                                      )
-
-def ray_train(save_in_sub_folder=None):
-    subprocess.run(["chmod", "-R", "a+rwx", save_in_sub_folder + "/"])
-    # Postprocess the perturbed config to ensure it's still valid
-
-
-
-    s3pathname = 's3://datastore-s3/groups/Behavior/Pinaki'                                 
-    upload_dir_path = s3pathname + "/" + ray_folder + '/' + InceptcurrentDT
-    if save_in_sub_folder is not None:
-        local_dir_path = save_in_sub_folder
-            #makedirpath(upload_dir_path)
-    '''ray_experiment = Experiment(name=None,
-                                run="PPO",
-                                stop={"training_iteration": int(num_timesteps)},
-                                checkpoint_at_end=True,
-                                checkpoint_freq=5,
-                                local_dir=local_dir_path,
-                                #upload_dir=upload_dir_path,
-                                config={
-                                            "num_gpus_per_worker": 0,
-                                            "num_cpus_per_worker": 2,
-                                            "gamma": 0.85,
-                                            "num_workers": 1,
-                                            "env": train_env_id,
-                                            # These params are tuned from a fixed starting value.
-                                            #"lambda": 0.95,
-                                            #"clip_param": 0.2,
-                                            #"lr": 1e-4,
-                                            # These params start off randomly drawn from a set.
-                                            #"num_sgd_iter": sample_from(lambda spec: random.choice([10, 20, 30])),
-                                            #"sgd_minibatch_size": sample_from(lambda spec: random.choice([128, 512, 2048])),
-                                            #"train_batch_size": sample_from(lambda spec: random.choice([10000, 20000, 40000])),
-                                       },
-                                )
-
-
-    run_experiments(ray_experiment,
-                    resume=False,
-                    reuse_actors=False,
-                    scheduler=pbt,
-                    verbose=False,
-                    ) '''
-
-    ray.tune.run(
-                    "PPO",
-                    name="pygame-ray",
-                    stop={"training_iteration": int(num_timesteps)},
-                    scheduler=pbt,
-                    checkpoint_freq=20,
-                    checkpoint_at_end=True,
-                    local_dir=local_dir_path,
-                    #upload_dir=upload_dir_path,
-                    verbose=True,
-                    queue_trials=False,
-                    resume=True,
-                    trial_executor=RayTrialExecutor(),
-                    **{
-                        #"env": train_env_id,
-                        "num_samples": 8,
-                        "config" :{
-                                    "num_gpus_per_worker": 0,
-                                    "num_cpus_per_worker": 1,
-                                    "gamma": 0.85,
-                                    "num_workers": 50,
-                                    "env": train_env_id,
-                                    # These params are tuned from a fixed starting value.
-                                    "lambda": 0.95,
-                                    "clip_param": 0.2,
-                                    "lr": 1e-4,
-                                    # These params start off randomly drawn from a set.
-                                    "num_sgd_iter": sample_from(lambda spec: random.choice([10, 20, 30])),
-                                    "sgd_minibatch_size": sample_from(lambda spec: random.choice([128, 512, 2048])),
-                                    "train_batch_size": sample_from(lambda spec: random.choice([10000, 20000, 40000])),
-                                },
-                    }
-                    
-
-                )                       
 
 
 if __name__ == "__main__":
@@ -251,6 +138,7 @@ if __name__ == "__main__":
                 #save_in_sub_folder = InceptcurrentDT
                 #args, args_dict = default_args(save_in_sub_folder=save_in_sub_folder)
                 #ray_train(save_in_sub_folder=args_dict['save_path'])
+                from raylibs import ray_train
                 ray_train(save_in_sub_folder=pathname + "/" + ray_folder + "/" + InceptcurrentDT)
             else:
                 print("(rank , size) = ", mpi_util.get_local_rank_size(MPI.COMM_WORLD))
