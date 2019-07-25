f478cb212dcc602051b82f0c438376f2fc44d082
pinakigupta <pinaki.gupta@havalus.com>
2019-07-25 10:56:45 -0400

    ray sync automatically reads the ray yaml file settings

--- ray_sync.sh
+++ ray_sync.sh
@@ -7,9 +7,25 @@ if [ $# \> 0 ]
     ray_yaml_file="Ray-Cluster.yaml"
 fi
 
+# Automatically parse the yaml file and get the remote mount path
+source yaml.sh
+yaml_key_val="$(parse_yaml "$ray_yaml_file" )"
+#echo $yaml_key_val
+while IFS=' ' read -ra ADDR; do
+     	for i in "${ADDR[@]}"; do
+              if [[ $i == *"/mnt"* ]]; then
+                 IFS=':' read -ra REMOTE_DOWNLOAD_PATH <<< "$i"
+              fi
+      done
+ done <<< "$yaml_key_val"
+REMOTE_DOWNLOAD_PATH="$REMOTE_DOWNLOAD_PATH/"
+
 LOCAL_DOWNLOAD_PATH="$PWD/ray_results/"
 echo "LOCAL_DOWNLOAD_PATH" $LOCAL_DOWNLOAD_PATH
 
-REMOTE_DOWNLOAD_PATH="/mnt/datastore/groups/behavior/Pinaki/rl_baselines/rl_baselines_ad/ray_results/"
+# You can manually override the remote mount path here 
+#REMOTE_DOWNLOAD_PATH="/mnt/datastore/groups/behavior/Pinaki/rl_baselines/rl_baselines_ad/ray_results/"  
+echo "REMOTE_DOWNLOAD_PATH" $REMOTE_DOWNLOAD_PATH
+
 ray exec $ray_yaml_file "sudo chmod -R a+rwx $REMOTE_DOWNLOAD_PATH"
 ray rsync_down $ray_yaml_file $REMOTE_DOWNLOAD_PATH $LOCAL_DOWNLOAD_PATH # rsync_up will upload
