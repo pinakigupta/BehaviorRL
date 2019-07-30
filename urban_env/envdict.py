2617f2452b1e4c4d0e7ca4735e5ee343736c2c71
pinakigupta <pinaki.gupta@havalus.com>
2019-07-30 15:43:17 -0400

    added the env dictionary .py file

---
+++ urban_env/envdict.py
@@ -0,0 +1,10 @@
+from urban_env import utils
+
+ACTIONS_DICT={0: 'LANE_LEFT',
+              1: 'IDLE',
+              2: 'LANE_RIGHT',
+              3: 'FASTER',
+              4: 'SLOWER',
+              5: 'LANE_LEFT_AGGRESSIVE',
+              6: 'LANE_RIGHT_AGGRESSIVE'
+              }
