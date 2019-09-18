87dfbe0921f742dce240f885e3c9b7a2c462cd35
pinakigupta <pinaki.gupta@havalus.com>
2019-09-18 19:45:04 -0400

    calling baseline files only when running baselines

--- baselines_run.py
+++ baselines_run.py
@@ -43,9 +43,7 @@ open_ai_baselines_dir = pathname + '/open_ai_baselines'
 sys.path.append(open_ai_baselines_dir)
 
 
-from baselines.common import tf_util, mpi_util
-from baselines.common.vec_env import VecEnv
-#import baselines.run as run
+
 
 ####################
 
@@ -76,7 +74,6 @@ def main(**kwargs):
             sess.close()
             tf.reset_default_graph()
             print(" Batch iteration ", mega_batch_itr)
-            #gym.Env.metadata['DIFFICULTY_LEVELS'] = mega_batch_itr
             print("(rank , size) = ", mpi_util.get_local_rank_size(MPI.COMM_WORLD))
 
             if RUN_WITH_RAY:
@@ -85,6 +82,9 @@ def main(**kwargs):
                 print("save_in_sub_folder is ", save_in_sub_folder)
                 ray_train(save_in_sub_folder=save_in_sub_folder)
             else:
+                from baselines.common import tf_util, mpi_util
+                from baselines.common.vec_env import VecEnv
+                #import baselines.run as run
                 print("(rank , size) = ", mpi_util.get_local_rank_size(MPI.COMM_WORLD))
                 if len(sys_args) <= 1:
                     save_in_sub_folder = None
@@ -99,10 +99,6 @@ def main(**kwargs):
             mega_batch_itr += 1
 
             play_env = gym.make(play_env_id)
-            '''try:
-                play(play_env, policy)
-            except Exception as e:
-                print("Could not play the prediction after training due to error  ", e)'''
             # from tensorboard import main as tb
             # tb.main()
 
@@ -113,6 +109,9 @@ def main(**kwargs):
             from raylibs import ray_play
             ray_play()
         else:
+            from baselines.common import tf_util, mpi_util
+            from baselines.common.vec_env import VecEnv
+            #import baselines.run as run
             from baselinelibs import baselines_play
             play_env = gym.make(play_env_id)
             DFLT_ARGS, _ = default_args()
