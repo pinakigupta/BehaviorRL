f939c887d82da70bb02bae98ec4d3b88088f8b86
pinakigupta <pinaki.gupta@havalus.com>
2019-09-18 22:11:40 -0400

    isolating baseline dependencies further so that ray docker doesn't need them

--- baselines_run.py
+++ baselines_run.py
@@ -1,3 +1,6 @@
+import gym
+from handle_model_files import train_env_id, play_env_id, alg, network, num_timesteps, homepath, RUN_WITH_RAY, InceptcurrentDT
+from handle_model_files import create_dirs, req_dirs, models_folder, makedirpath, is_master, is_predict_only, default_args
 import urban_env
 from urban_env.envs.two_way_env import TwoWayEnv
 from urban_env.envs.abstract import AbstractEnv
@@ -21,6 +24,7 @@ from color import color
 def exit_handler():
     subprocess.run(["chmod", "-R", "a+rwx", "."])
 
+
 atexit.register(exit_handler)
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -43,18 +47,12 @@ open_ai_baselines_dir = pathname + '/open_ai_baselines'
 sys.path.append(open_ai_baselines_dir)
 
 
-
-
 ####################
 
 
+# gym.Env.metadata['_predict_only'] = is_predict_only()
 
-from handle_model_files import create_dirs, req_dirs, models_folder, makedirpath, is_master, is_predict_only, default_args
-from handle_model_files import train_env_id, play_env_id, alg, network, num_timesteps, homepath, RUN_WITH_RAY, InceptcurrentDT
-import gym
-#gym.Env.metadata['_predict_only'] = is_predict_only()
-
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.filterwarnings("ignore")
 
 #################################################################
@@ -64,45 +62,46 @@ def main(**kwargs):
     predict_only = is_predict_only(**kwargs)
     mega_batch_itr = 1
     sys_args = sys.argv
-    
+
     policy = None
     play_env = None
     max_iteration = 1
     if not predict_only:
-        while mega_batch_itr <= max_iteration:
-            sess = tf_util.get_session()
-            sess.close()
-            tf.reset_default_graph()
-            print(" Batch iteration ", mega_batch_itr)
-            print("(rank , size) = ", mpi_util.get_local_rank_size(MPI.COMM_WORLD))
-
-            if RUN_WITH_RAY:
-                from raylibs import ray_train
-                save_in_sub_folder = pathname + "/" + ray_folder + "/" + InceptcurrentDT
-                print("save_in_sub_folder is ", save_in_sub_folder)
-                ray_train(save_in_sub_folder=save_in_sub_folder)
-            else:
+        if RUN_WITH_RAY:
+            from raylibs import ray_train
+            save_in_sub_folder = pathname + "/" + ray_folder + "/" + InceptcurrentDT
+            print("save_in_sub_folder is ", save_in_sub_folder)
+            ray_train(save_in_sub_folder=save_in_sub_folder)
+        else:
+            while mega_batch_itr <= max_iteration:
                 from baselines.common import tf_util, mpi_util
                 from baselines.common.vec_env import VecEnv
-                #import baselines.run as run
+
+                sess = tf_util.get_session()
+                sess.close()
+                tf.reset_default_graph()
+                print(" Batch iteration ", mega_batch_itr)
                 print("(rank , size) = ", mpi_util.get_local_rank_size(MPI.COMM_WORLD))
+
+                # import baselines.run as run
+                print("(rank , size) = ",mpi_util.get_local_rank_size(MPI.COMM_WORLD))
                 if len(sys_args) <= 1:
                     save_in_sub_folder = None
                     if max_iteration > 1:
                         save_in_sub_folder = InceptcurrentDT
-                    args, args_dict = default_args(save_in_sub_folder=save_in_sub_folder)
+                    args, args_dict = default_args(
+                        save_in_sub_folder=save_in_sub_folder)
                 policy = run.main(args)
                 MPI.COMM_WORLD.barrier()
 
+                # print("policy training args ", args,"\n\n")
+                mega_batch_itr += 1
 
-            # print("policy training args ", args,"\n\n")
-            mega_batch_itr += 1
+                play_env = gym.make(play_env_id)
+                # from tensorboard import main as tb
+                # tb.main()
 
-            play_env = gym.make(play_env_id)
-            # from tensorboard import main as tb
-            # tb.main()
-
-            print(color.BOLD + 'Successfully ended Training!' + color.END)
+        print(color.BOLD + 'Successfully ended Training!' + color.END)
 
     else:
         if RUN_WITH_RAY:
@@ -111,11 +110,12 @@ def main(**kwargs):
         else:
             from baselines.common import tf_util, mpi_util
             from baselines.common.vec_env import VecEnv
-            #import baselines.run as run
+            # import baselines.run as run
             from baselinelibs import baselines_play
             play_env = gym.make(play_env_id)
             DFLT_ARGS, _ = default_args()
-            loaded_file_correctly = ('load_path' in stringarg for stringarg in DFLT_ARGS)
+            loaded_file_correctly = (
+                'load_path' in stringarg for stringarg in DFLT_ARGS)
             policy = run.main(DFLT_ARGS)
             # Just try to Play
             while loaded_file_correctly:
