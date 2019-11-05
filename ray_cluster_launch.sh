1219134f5699a8de06cf9da3b2658d4e6b776338
pinakigupta <pinaki.gupta@havalus.com>
2019-11-05 16:33:37 -0500

    Trying to pass cluster information from the local docker container

--- ray_cluster_launch.sh
+++ ray_cluster_launch.sh
@@ -7,13 +7,25 @@ if [ $# \> 0 ]
     ray_yaml_file="Ray-Cluster.yaml" # Your default cluster yaml file
 fi
 
+# Automatically parse the yaml file and get the remote mount path
+source yaml.sh
+yaml_key_val="$(parse_yaml "$ray_yaml_file" )" #parse_yaml script parses the ray yaml file
+while IFS=' ' read -ra ADDR; do
+     	for i in "${ADDR[@]}"; do
+              if [[ $i == *"min_workers"* ]]; then
+                 IFS=':' read -ra min_cluster_nodes <<< "$i"
+              fi
+      done
+ done <<< "$yaml_key_val"
+
+
 if [ $# \> 1 ]
   then
     exec_cmd=$2
   else
-    exec_cmd="cd BehaviorRL;./run_baseline.sh" # Your default ray exec cmnd
+    exec_cmd="cd BehaviorRL;python -W ignore baselines_run.py min_cluster_nodes=$min_cluster_nodes init_cluster_nodes=$init_cluster_nodes " # Your default ray exec cmnd
 fi
-
+ 
 #copying from the volatile docker container to the persistent mount drive
 sync_cmd="rsync -a  /BehaviorRL/ray_results/   /rl_baselines_ad/ray_results/" 
 
