0d3bd83f8dd084dbb06d766b9ea42c28e016ce9e
pinakigupta <pinaki.gupta@havalus.com>
2019-12-18 15:41:50 -0500

    Deleting local lgsvl directory. Will directly refer to the PythonAPI/lgsvl from now

--- urban_env/envs/lgsvl/__init__.py
+++
@@ -1,5 +0,0 @@
-from .geometry import Vector, BoundingBox, Transform
-from .simulator import Simulator, RaycastHit
-from .sensor import Sensor, CameraSensor, LidarSensor, ImuSensor
-from .agent import AgentType, VehicleControl, AgentState, Vehicle, EgoVehicle, NpcVehicle, Pedestrian
-
